50ebd8a66e21036c6f420905e9a59a39687a7ffa
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-10-28 22:07:21 +0200

    [WIP]

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // typescript: {
-    //     // !! WARN !!
-    //     // Dangerously allow production builds to successfully complete even if
-    //     // your project has type errors.
-    //     // !! WARN !!
-    //     ignoreBuildErrors: true,
-    //   } 
+  // Workaround for 
+
+  // BUG
+  // does not match the required types of a Next.js Route.
+  // "authOptions" is not a valid Route export field.
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      } 
 }
 
 module.exports = nextConfig
