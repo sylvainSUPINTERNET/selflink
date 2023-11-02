b4718336018fa742aa7f8e6e2faf9bec1bd97c2b
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-11-02 21:39:22 +0100

    [UPDATE]

--- app/components/dashboard/orders.tsx
+++ app/components/dashboard/orders.tsx
@@ -222,24 +222,67 @@ export const OrdersList = ({paymentLinkInit, paymentLinkInitUrl, offset, setOffs
                 </div> 
             </div>
         </dialog>
-            
+
+
         <div className="mt-2 p-2 mb-3">
+                
+                <div className="flex flex-col sm:flex-row justify-center items-center space-y-2 sm:space-y-0 sm:space-x-4 mb-4">
+                    {tagged.length > 0 ? (
+                    <button
+                        className="btn-primary text-white font-bold py-2 px-4 rounded-lg w-full sm:w-auto transition duration-300 ease-in-out transform hover:scale-105 shadow-lg"
+                        onClick={e => {
+                        saveOrderStatus();
+                        }}
+                    >
+                        Sauvegarder
+                    </button>
+                    ) : (
+                    <button
+                        className="bg-gray-300 text-white font-bold py-2 px-4 rounded-lg w-full sm:w-auto cursor-not-allowed"
+                        disabled
+                    >
+                        Sauvegarder
+                    </button>
+                    )}
 
-        <div className="flex justify-center  p-0.5 text-2xl">
-                        {tagged.length > 0 ? 
-            <button className="btn btn-primary text-white  w-2/4" onClick={e => {
-                saveOrderStatus()
-            }}>Sauvegarder</button>
-            : 
-            <button className="btn btn-disabled text-white  w-2/4">Sauvegarder</button>
-            }
-        </div>
+                    {/* Payment Link */}
+                    {
+                    stock > 0 ?
+                    <a 
+                        className="btn-primary  text-white font-bold py-2 px-4 rounded-lg w-full sm:w-auto transition duration-300 ease-in-out transform hover:scale-105 shadow-lg flex justify-center items-center" 
+                        target="_blank" 
+                        href={paymentLinkInitUrl}
+                    >
+                        Voir le lien
+                    </a>
+                    :
+                    <a 
+                        className="bg-gray-300 text-white font-bold py-2 px-4 rounded-lg w-full sm:w-auto cursor-not-allowed flex justify-center items-center" 
+                    >
+                        {/* SVG icon and text */}
+                        {/* SVG code remains the same */}
+                        Lien désactivé { stock === 0 ? " (stock épuisé)" : ""}
+                    </a>
+                    }
+                </div>
+                </div>
+
             
+        {/* <div className="mt-2 p-2 mb-3">
 
-            
-        </div>
+            <div className="flex justify-center  p-0.5 text-2xl">
+                            {tagged.length > 0 ? 
+                <button className="btn btn-primary text-white  w-2/4" onClick={e => {
+                    saveOrderStatus()
+                }}>Sauvegarder</button>
+                : 
+                <button className="btn btn-disabled text-white  w-2/4">Sauvegarder</button>
+                }
+            </div>
+        
+        </div> */}
 
-        <div className="flex justify-center  mb-4">
+        {/* <div className="flex justify-center  mb-4">
                     {
                         stock > 0 ?
                         <a className="btn btn-primary text-white" target="blank" href={paymentLinkInitUrl}>
@@ -250,14 +293,50 @@ export const OrdersList = ({paymentLinkInit, paymentLinkInitUrl, offset, setOffs
                         <a className="btn btn-disabled " >
                             <svg fill="#000000" version="1.1" id="Capa_1" className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 442.04 442.04"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M221.02,341.304c-49.708,0-103.206-19.44-154.71-56.22C27.808,257.59,4.044,230.351,3.051,229.203 c-4.068-4.697-4.068-11.669,0-16.367c0.993-1.146,24.756-28.387,63.259-55.881c51.505-36.777,105.003-56.219,154.71-56.219 c49.708,0,103.207,19.441,154.71,56.219c38.502,27.494,62.266,54.734,63.259,55.881c4.068,4.697,4.068,11.669,0,16.367 c-0.993,1.146-24.756,28.387-63.259,55.881C324.227,321.863,270.729,341.304,221.02,341.304z M29.638,221.021 c9.61,9.799,27.747,27.03,51.694,44.071c32.83,23.361,83.714,51.212,139.688,51.212s106.859-27.851,139.688-51.212 c23.944-17.038,42.082-34.271,51.694-44.071c-9.609-9.799-27.747-27.03-51.694-44.071 c-32.829-23.362-83.714-51.212-139.688-51.212s-106.858,27.85-139.688,51.212C57.388,193.988,39.25,211.219,29.638,221.021z"></path> </g> <g> <path d="M221.02,298.521c-42.734,0-77.5-34.767-77.5-77.5c0-42.733,34.766-77.5,77.5-77.5c18.794,0,36.924,6.814,51.048,19.188 c5.193,4.549,5.715,12.446,1.166,17.639c-4.549,5.193-12.447,5.714-17.639,1.166c-9.564-8.379-21.844-12.993-34.576-12.993 c-28.949,0-52.5,23.552-52.5,52.5s23.551,52.5,52.5,52.5c28.95,0,52.5-23.552,52.5-52.5c0-6.903,5.597-12.5,12.5-12.5 s12.5,5.597,12.5,12.5C298.521,263.754,263.754,298.521,221.02,298.521z"></path> </g> <g> <path d="M221.02,246.021c-13.785,0-25-11.215-25-25s11.215-25,25-25c13.786,0,25,11.215,25,25S234.806,246.021,221.02,246.021z"></path> </g> </g> </g></svg>
                                 Lien desactivé { stock === 0 ? " (stock épuisé)" : ""}
-                        </a>
-                    
-                    
+                        </a>                    
                     }
+        </div> */}
+
+        <div>
+
         </div>
 
         {
+            paymentLinkInitUrl && (
+                <div className="flex justify-center space-x-4 mb-5 md:mb-0">
+                {/* TOTAL Price Tag */}
+                <div className="flex items-center">
+                    <span className="inline-block rounded-full bg-indigo-100 px-3 py-1 text-sm font-semibold text-indigo-800 mr-2 ring ring-indigo-200">Total</span>
+                    <span className="text-sm font-medium">
+                    {new Intl.NumberFormat('fr-FR', {
+                        style: 'currency',
+                        currency: 'EUR',
+                    }).format(amountEstimate)}
+                    </span>
+                </div>
+
+                {/* STOCK Information Tag */}
+                {stock > 0 ? (
+                    <div className="flex items-center">
+                    <span className="inline-block rounded-full bg-green-100 px-3 py-1 text-sm font-semibold text-green-800 mr-2 ring ring-green-200">Stock</span>
+                    <span className="text-sm">{stock}</span>
+                    </div>
+                ) : (
+                    <div className="flex items-center shadow-md">
+                    <span className="inline-block rounded-full bg-red-100 px-3 py-1 text-sm font-semibold text-red-800 mr-2 ring ring-red-200">Rupture</span>
+                    </div>
+                )}
+                </div>
+            )
+        }
+
+
+
+
+
+        {/* {
                 paymentLinkInitUrl ? <>
+
                 
                     <div className="justify-center flex space-x-3 mb-5 md:mb-0">
                         <div className="">
@@ -298,10 +377,11 @@ export const OrdersList = ({paymentLinkInit, paymentLinkInitUrl, offset, setOffs
                         </div>
                     </div>
                     
+
                  </>
                 : 
                 <></>
-            }
+            } */}
             
             {
                 orderCount ? 
